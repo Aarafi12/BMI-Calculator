a4d10b92b8647e11669bb00151a869ae84d1ad0f
Aroj Amin Rafi <arojamin00@gmail.com>
2023-07-03 02:07:07 -0230

    1st commit

---
+++ app/src/main/java/com/example/mybmicalculator/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mybmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"I can show an alert pop up!",Toast.LENGTH_LONG).show();
+
+    }
+}
